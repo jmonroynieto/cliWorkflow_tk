441eb9b45427f155fa16d15a6c2b3b09d2f2072e
Juan Monroy-Nieto <jmonroynieto@outlook.com>
2025-06-07 20:19:11 -0500

    FICKLEFINGER: enable library use

--- fickleFinger/id.go
+++ fickleFinger/id.go
@@ -3,49 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/rand/v2"
 
+	"github.com/jmonroynieto/cliWorkflow_tk/fickleFinger/idGen"
 	"github.com/urfave/cli/v3"
 )
 
-var (
-	n         int64 // number of characters to generate
-	noNewline bool
-)
-
 func GenerateID(cCtx context.Context, cmd *cli.Command) error {
-
-	var b = make([]byte, 0, n)
-	var formatTemplate string
-	for len(b) < int(n) {
-		num := rand.N(122)
-		if isInRange(num) {
-			b = append(b, byte(num))
-		}
-	}
-	if noNewline {
-		formatTemplate = "%s"
-	} else {
-		formatTemplate = "%s\n"
-	}
-	fmt.Printf(formatTemplate, string(b))
+	n := cmd.Int("length")
+	noNewline := cmd.Bool("no_newline")
+	fmt.Print(idGen.New(n, noNewline))
 	return nil
 
-}
-
-func isInRange(n int) bool {
-	ranges := [][2]int{
-		{48, 57},  // 0-9
-		{65, 90},  // A-Z
-		{97, 122}, // a-z
-	}
-
-	for _, r := range ranges {
-		if n >= r[0] && n <= r[1] {
-			return true
-		}
-	}
-	return false
 }
 
 var idFlags []cli.Flag = []cli.Flag{
@@ -53,7 +21,6 @@ var idFlags []cli.Flag = []cli.Flag{
 		Name:        "length",
 		Aliases:     []string{"l"},
 		Usage:       "length of the id",
-		Destination: &n,
 		Required:    false,
 		Value:       11,
 	},
@@ -61,11 +28,5 @@ var idFlags []cli.Flag = []cli.Flag{
 		Name:        "no_newline",
 		Aliases:     []string{"n"},
 		Usage:       "no new line at the end of the id",
-		Destination: &noNewline,
 	},
-}
-
-func ParamSet(nn int64, lib_noNewline bool) {
-	n = nn
-	noNewline = lib_noNewline
 }
