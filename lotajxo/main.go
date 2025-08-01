f81e605268c7bc5e185695fcb58f0716e4192133
Juan Monroy-Nieto <jmonroynieto@outlook.com>
2025-08-01 15:01:31 -0500

    Chore(ljx):code cleanup

--- lotajxo/main.go
+++ lotajxo/main.go
@@ -12,28 +12,18 @@ var (
 	Revision = ".0"
 )
 
-// ANSI escape codes for neon bright colors
 const (
 	ColorReset   = "\033[0m"
 	ColorRed     = "\033[1;31m" // Bright Red
 	ColorGreen   = "\033[1;32m" // Bright Green
-	ColorYellow  = "\033[1;33m" // Bright Yellow
-	ColorBlue    = "\033[1;34m" // Bright Blue
 	ColorMagenta = "\033[1;35m" // Bright Magenta
 	ColorCyan    = "\033[1;36m" // Bright Cyan
 )
 
-// generateAndReflectRandomUint8V2 generates a random uint8 and prints its
-// decimal, hexadecimal, and binary representations using math/rand/v2.
+.
 func generateAndReflectRandomUint8V2() {
-	// Generate a random uint8.
-	// rand.IntN(256) returns a non-negative pseudo-random number in the half-open interval [0, 256).
-	// Casting it to uint8 ensures it fits within the 0-255 range.
 	randomNum := uint8(rand.IntN(256))
 
-	// Print all representations on a single line with ANSI colorization.
-	// Each representation gets a different bright color.
-	// ColorReset is used to reset the color after each part to avoid bleeding into the next.
 	fmt.Printf("Generated random uint8: %s%d%s (Decimal) | %s0x%x%s (Hex) | %s0b%08b%s (Binary)\n",
 		ColorGreen, randomNum, ColorReset, // Decimal in Green
 		ColorCyan, randomNum, ColorReset, // Hex in Cyan
@@ -41,14 +31,14 @@ func generateAndReflectRandomUint8V2() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		os.Args = append(os.Args, "notAFlag")
-	}
-	switch os.Args[1] {
-	case "--version", "-v", "version":
-		fmt.Printf("%s%s (%s)\n", Version, Revision, CommitId)
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v" || os.Args[1] == "version") {
+		fmt.Printf("lotajxo %s%s (%s)\n", Version, Revision, CommitId)
 		return
-	default:
-		generateAndReflectRandomUint8V2()
+	} else if len(os.Args) > 1 && (os.Args[1] == "--help" || os.Args[1] == "-h" || os.Args[1] == "help") {
+		// TODO: Add help message
+		fmt.Printf("random uint8 generator simultanious representations in decimal, hexadecimal, and binary\n\t --version, -v, version\n\t --help, -h, this help message\n")
+	} else if len(os.Args) > 1 {
+		fmt.Printf("%sunrecognized arguments:%s %s\n", ColorRed,os.Args[1:], ColorReset)
 	}
+	generateAndReflectRandomUint8V2()
 }
